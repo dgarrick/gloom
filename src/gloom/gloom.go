199840e17db06e33c577ebd59f30513b65319368
Daniel Garrick <danieljamesgarrick@gmail.com>
2016-08-21 21:47:55 -0500

    minor changes, mostly polish.

--- src/gloom/gloom.go
+++ src/gloom/gloom.go
@@ -9,9 +9,9 @@ import (
 )
 
 type BloomFilter struct {
-   m             int //number of chunks in filter
+   m             int //number of bits in filter
    k             int //number of hash functions
-   size          int //number of elements to be inserted in set 
+   size          int //number of elements to be inserted in filter
    chunks        []uint64
    hash          hash.Hash64
 }
@@ -20,8 +20,8 @@ func NewFilter(size int, fpos float64) *BloomFilter {
   if size < 1 || fpos <= 0.0 || fpos >= 1.0 {
     panic("Invalid parameters for Bloom Filter! Size must be > 1 and 0.0 < fpos < 1.0!")
   }
-  m := GetOptimalM(size, fpos)
-  k := GetOptimalK(m, size)
+  m := OptimalM(size, fpos)
+  k := OptimalK(m, size)
   numchunks := m / 64
   if m % 64 > 0 {
     numchunks++
@@ -34,26 +34,23 @@ func (bf *BloomFilter) EstimateFalsePos() float64 {
   return math.Pow(1.0 - math.Exp((-1.0 * float64(bf.k * bf.size)) / float64(bf.m)),  float64(bf.k))
 }
 
-func GetOptimalM (size int, fpos float64) int {
-  c := math.Pow(math.Log(2.0),2.0)
-  return int(math.Ceil((float64(-1 * size) * math.Log(fpos)) / c))
+func OptimalM (size int, fpos float64) int {
+  return int(math.Ceil((float64(-1 * size) * math.Log(fpos)) / math.Pow(math.Log(2.0),2.0)))
 }
 
-func GetOptimalK (m int, size int) int {
+func OptimalK (m int, size int) int {
   return int(math.Ceil(float64(m) / float64(size) * math.Log(2.0)))
 }
 
 func (bf * BloomFilter) GetHashes(key []byte) []uint64 {
   bf.hash.Sum(key)
   h := bf.hash.Sum64()
-  h1 := uint32(h)
-  h2 := uint32(h >> 32) 
   hs := make([]uint64,bf.k)
   for i := 0; i < bf.k; i++ {
    //This distributes values so that one hash function
    //can effectively act as k hash functions 
-   //https://www.eecs.harvard.edu/~michaelm/postscripts/rsa2008.pdf
-   hs[i] = uint64(h1 + uint32(i)*h2)
+   //for more info, see: Kirsch and Mitzenmacher 
+   hs[i] = uint64(uint32(h) + uint32(i) * uint32(h >> 32))
   }
   return hs
 }
