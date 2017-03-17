61e5c45ee9fc43e61c4489a6c5876fc10862698a
Daniel Garrick <danieljamesgarrick@gmail.com>
2017-03-17 00:38:15 -0500

    Update gloom.go

--- src/gloom/gloom.go
+++ src/gloom/gloom.go
@@ -50,6 +50,8 @@ func (bf *BloomFilter) getHashes(key []byte) []uint64 {
 		//This distributes values so that one hash function
 		//can effectively act as k hash functions
 		//for more info, see: Kirsch and Mitzenmacher
+		//full disclosure: I discovered this tidbit from the Bloom Filter wikipedia entry:
+		//https://en.wikipedia.org/wiki/Bloom_filter#cite_ref-3
 		hs[i] = uint64(uint32(h) + uint32(i)*uint32(h>>32))
 	}
 	bf.hash.Reset()
