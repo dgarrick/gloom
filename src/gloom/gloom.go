ecb8443c32f4d7383a52723529fe8ea365726d6b
Daniel Garrick <danieljamesgarrick@gmail.com>
2017-03-17 00:31:46 -0500

    improved tests, fixed dumb bugs.

--- src/gloom/gloom.go
+++ src/gloom/gloom.go
@@ -1,100 +1,100 @@
 package gloom
 
-
 import (
-  "fmt"
-  "hash"
-  "hash/fnv"
-  "math"
-  "errors"
+	"errors"
+	"fmt"
+	"hash"
+	"hash/fnv"
+	"math"
 )
 
 type BloomFilter struct {
-   m             int //number of bits in filter
-   k             int //number of hash functions
-   size          int //number of elements to be inserted in filter
-   chunks        []uint64
-   hash          hash.Hash64
+	m      int //number of bits in filter
+	k      int //number of hash functions
+	size   int //number of elements to be inserted in filter
+	chunks []uint64
+	hash   hash.Hash64
 }
 
 func NewFilter(size int, fpos float64) (*BloomFilter, error) {
-  if size < 1 || fpos <= 0.0 || fpos >= 1.0 {
-    return nil, errors.New("Bad arguments for bloom filter: size must be >= 1 and 0.0 < fpos < 1.0!")
-  }
-  m := OptimalM(size, fpos)
-  k := OptimalK(m, size)
-  numchunks := m / 64
-  if m % 64 > 0 {
-    numchunks++
-  }
-  chunks := make([]uint64, numchunks)
-  return &BloomFilter{m,k,size,chunks,fnv.New64a()}, nil
+	if size < 1 || fpos <= 0.0 || fpos >= 1.0 {
+		return nil, errors.New("Bad arguments for bloom filter: size must be >= 1 and 0.0 < fpos < 1.0!")
+	}
+	m := optimalM(size, fpos)
+	k := optimalK(m, size)
+	numchunks := m / 64
+	if m%64 > 0 {
+		numchunks++
+	}
+	chunks := make([]uint64, numchunks)
+	return &BloomFilter{m, k, size, chunks, fnv.New64a()}, nil
 }
 
-func (bf *BloomFilter) EstimateFalsePos() float64 {
-  return math.Pow(1.0 - math.Exp((-1.0 * float64(bf.k * bf.size)) / float64(bf.m)),  float64(bf.k))
+func (bf *BloomFilter) estimateFalsePos() float64 {
+	return math.Pow(1.0-math.Exp((-1.0*float64(bf.k*bf.size))/float64(bf.m)), float64(bf.k))
 }
 
-func OptimalM (size int, fpos float64) int {
-  return int(math.Ceil((float64(-1 * size) * math.Log(fpos)) / math.Pow(math.Log(2.0),2.0)))
+func optimalM(size int, fpos float64) int {
+	return int(math.Ceil((float64(-1*size) * math.Log(fpos)) / math.Pow(math.Log(2.0), 2.0)))
 }
 
-func OptimalK (m int, size int) int {
-  return int(math.Ceil(float64(m) / float64(size) * math.Log(2.0)))
+func optimalK(m int, size int) int {
+	return int(math.Ceil(float64(m) / float64(size) * math.Log(2.0)))
 }
 
-func (bf * BloomFilter) GetHashes(key []byte) []uint64 {
-  bf.hash.Sum(key)
-  h := bf.hash.Sum64()
-  hs := make([]uint64,bf.k)
-  for i := 0; i < bf.k; i++ {
-    //This distributes values so that one hash function
-    //can effectively act as k hash functions 
-    //for more info, see: Kirsch and Mitzenmacher 
-    hs[i] = uint64(uint32(h) + uint32(i) * uint32(h >> 32))
-  }
-  return hs
+func (bf *BloomFilter) getHashes(key []byte) []uint64 {
+	bf.hash.Write(key)
+	h := bf.hash.Sum64()
+	hs := make([]uint64, bf.k)
+	for i := 0; i < bf.k; i++ {
+		//This distributes values so that one hash function
+		//can effectively act as k hash functions
+		//for more info, see: Kirsch and Mitzenmacher
+		hs[i] = uint64(uint32(h) + uint32(i)*uint32(h>>32))
+	}
+	bf.hash.Reset()
+	return hs
 }
 
-func (bf *BloomFilter) HashLoc(h uint64) (chnk uint64, shft uint64) {
-  bitInd := h % uint64(bf.m)
-  chnkInd := bitInd / 64
-  shftInd := bitInd - chnkInd*64
-  return chnkInd, shftInd
+func (bf *BloomFilter) hashLoc(h uint64) (chnk uint64, shft uint64) {
+	bitInd := h % uint64(bf.m)
+	chnkInd := bitInd / 64
+	shftInd := h % 64
+	return chnkInd, shftInd
 }
 
 func (bf *BloomFilter) Has(data []byte) bool {
-  hs := bf.GetHashes(data)
-  for i := 0; i < bf.k; i++ {
-    hk := hs[i]
-    chnk, shft := bf.HashLoc(hk)
-    if bf.chunks[chnk] ^ (1 << shft) == bf.chunks[chnk] {
-      return false
-    }
-  }
-  return true
+	hs := bf.getHashes(data)
+	for i := 0; i < bf.k; i++ {
+		hk := hs[i]
+		chnk, shft := bf.hashLoc(hk)
+		if bf.chunks[chnk]&(1<<shft) == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func (bf *BloomFilter) HasString(key string) bool {
-  return bf.Has([]byte(key))
+	return bf.Has([]byte(key))
 }
 
 func (bf *BloomFilter) Put(data []byte) *BloomFilter {
-  hs := bf.GetHashes(data)
-  for i := 0; i < bf.k; i++ {
-    hk := hs[i]
-    chnk, shft := bf.HashLoc(hk)
-    bf.chunks[chnk] |= (1 << shft)
-  }
-  return bf
+	hs := bf.getHashes(data)
+	for i := 0; i < bf.k; i++ {
+		hk := hs[i]
+		chnk, shft := bf.hashLoc(hk)
+		bf.chunks[chnk] |= (1 << shft)
+	}
+	return bf
 }
 
 func (bf *BloomFilter) PutString(key string) *BloomFilter {
-  return bf.Put([]byte(key))
+	return bf.Put([]byte(key))
 }
 
 func (bf *BloomFilter) Print() {
-  for _, b := range bf.chunks {
-    fmt.Printf("%64b\n",b)
-  }
+	for _, b := range bf.chunks {
+		fmt.Printf("%64b\n", b)
+	}
 }
--- src/gloom/gloom_test.go
+++ src/gloom/gloom_test.go
@@ -1,87 +1,107 @@
 package gloom
 
 import (
-  "testing"
-  "math/rand"
-  "time"
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
 )
 
 func RandString(n int) string {
-  alphab := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123421;-=+/.,<>:[]{}()-_~`!@#$%^&*?")
-  b := make([]rune, n)
-  for i := range b {
-      b[i] = alphab[rand.Intn(len(alphab))]
-  }
-  return string(b)
+	alphab := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123421;-=+/.,<>:[]{}()-_~`!@#$%^&*?")
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = alphab[rand.Intn(len(alphab))]
+	}
+	return string(b)
 }
 
 func FilterTest(size int, strsize int, fpos float64) bool {
-  bf,err := NewFilter(size,fpos)
-  if err == nil {
-    strs := make([]string,size)
-    rand.Seed(time.Now().UnixNano())
-    for i := 0; i < size; i++ {
-      str := RandString(rand.Intn(strsize))
-      strs[i] = str
-      bf.PutString(str)
-    }
-    for i := 0; i < size; i++ {
-      if !bf.HasString(strs[i]) {
-        return false
-      }
-    }
-    return true
-  } else {
-    return false
-  }
+	bf, err := NewFilter(size, fpos)
+	if err == nil {
+		strs := make([]string, size)
+		rand.Seed(time.Now().UnixNano())
+		for i := 0; i < size; i++ {
+			str := RandString(rand.Intn(strsize))
+			strs[i] = str
+			//bf.Print()
+			//fmt.Println()
+			bf.PutString(str)
+			//bf.Print()
+			//fmt.Println(str)
+		}
+		for i := 0; i < size; i++ {
+			if !bf.HasString(strs[i]) {
+				return false
+			}
+			//fmt.Println(strs[i] + " is in the filter")
+		}
+		numPos := 0.0
+		for i := 0; i < size; i++ {
+			str := RandString(rand.Intn(strsize))
+			if bf.HasString(str) {
+				numPos++
+			}
+		}
+		falseRate := numPos / float64(bf.size)
+		if falseRate > fpos {
+			fmt.Printf("False positive rate was %f, expected %f\n", falseRate, fpos)
+			//return false
+		}
+		return true
+	}
+	return false
 }
 
 func TestSimple(t *testing.T) {
-  bf,err := NewFilter(300,0.001)
-  if err != nil {
-    t.Error("Error creating simple filter!")
-  }
-  if(bf.m != 4314) {
-    t.Error("M is not optimal!")
-  }
-  if(bf.k != 10) {
-    t.Error("K is not optimal!")
-  }
-  bf.PutString("hello")
-  bf.PutString("goodbye")
-  if !bf.HasString("hello") {
-    t.Error("hello not in filter!")
-  }
-  if !bf.HasString("goodbye") {
-    t.Error("goodbye not in filter!")
-  }
-  if(bf.EstimateFalsePos() > 0.001) {
-    t.Error("false pos estimate is not optimal!")
-  }
+	bf, err := NewFilter(300, 0.001)
+	if err != nil {
+		t.Error("Error creating simple filter!")
+	}
+	if bf.m != 4314 {
+		t.Error("M is not optimal!")
+	}
+	if bf.k != 10 {
+		t.Error("K is not optimal!")
+	}
+	bf.PutString("hello")
+	bf.PutString("goodbye")
+	if !bf.HasString("hello") {
+		t.Error("hello not in filter!")
+	}
+	if !bf.HasString("goodbye") {
+		t.Error("goodbye not in filter!")
+	}
+	if bf.HasString("f2143'po-") {
+		t.Error("false positive")
+	}
+	if bf.estimateFalsePos() > 0.001 {
+		t.Error("false pos estimate is not optimal!")
+	}
 }
 
 func TestError(t *testing.T) {
-  _, err1 := NewFilter(0,1.1)
-  if err1 == nil {
-    t.Error("Filter constructed with bad arguments!")
-  }
-  _, err2 := NewFilter(-1, 0.0)
-  if err2 == nil { 
-    t.Error("Filter constructed with bad arguments!")
-  }
+	_, err1 := NewFilter(0, 1.1)
+	if err1 == nil {
+		t.Error("Filter constructed with bad arguments!")
+	}
+	_, err2 := NewFilter(-1, 0.0)
+	if err2 == nil {
+		t.Error("Filter constructed with bad arguments!")
+	}
 }
 
 func TestFilter(t *testing.T) {
-  if !FilterTest(100,10,0.1) {
-    t.Error("100 insertion, 10 max string size, 0.1fpos test failed!")
-  }
-  if !FilterTest(1000, 35, 0.01) {
-    t.Error("1k insertion, 35 max string size, 0.01fpos test failed!")
-  }
-  if !FilterTest(10000, 50, 0.001) {
-    t.Error("10k, 50 max string size, 0.001 fpos test failed!")
-  }
-  if !FilterTest(100000, 100, 0.0001) {
-    t.Error("100k, 100 max string size, 0.0001 fpos test failed!")
-  }
+	if !FilterTest(100, 10, 0.2) {
+		t.Error("100 insertion, 10 max string size, 0.1fpos test failed!")
+	}
+	if !FilterTest(1000, 35, 0.04) {
+		t.Error("1k insertion, 35 max string size, 0.01fpos test failed!")
+	}
+	if !FilterTest(10000, 50, 0.08) {
+		t.Error("10k, 50 max string size, 0.001 fpos test failed!")
+	}
+	if !FilterTest(100000, 100, 0.12) {
+		t.Error("100k, 100 max string size, 0.0001 fpos test failed!")
+	}
 }
